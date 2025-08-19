44134cd73e6261f8606068159a4cc0b2a55d65f4
Andres De la Ossa <adelaossa0129@gmail.com>
2025-08-19 11:42:50 -0500

    feat: dto support tested in hybrid app

--- apps-examples/advanced-hybrid-crud-app/src/clients/dto/inputs/create-client.dto.ts
+++ apps-examples/advanced-hybrid-crud-app/src/clients/dto/inputs/create-client.dto.ts
@@ -1,44 +1,6 @@
-import { SolidInput, SolidField } from '@solid-nestjs/common';
+import { SolidInput } from '@solid-nestjs/typeorm-hybrid-crud';
+import { GenerateDtoFromEntity } from '@solid-nestjs/rest-graphql';
+import { Client } from '../../entities/client.entity';
 
 @SolidInput()
-export class CreateClientDto {
-  @SolidField({
-    description: 'The first name of the client'
-  })
-  firstName: string;
-
-  @SolidField({
-    description: 'The last name of the client'
-  })
-  lastName: string;
-
-  @SolidField({
-    description: 'The email address of the client',
-    email: true
-  })
-  email: string;
-
-  @SolidField({
-    description: 'The phone number of the client',
-    nullable: true
-  })
-  phone?: string;
-
-  @SolidField({
-    description: 'The address of the client',
-    nullable: true
-  })
-  address?: string;
-
-  @SolidField({
-    description: 'The city of the client',
-    nullable: true
-  })
-  city?: string;
-
-  @SolidField({
-    description: 'The country of the client',
-    nullable: true
-  })
-  country?: string;
-}
+export class CreateClientDto extends GenerateDtoFromEntity(Client) {}
