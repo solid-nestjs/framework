cf731661a89d7d103158034ac19e01d243f1c7f3
Andres De la Ossa <adelaossa0129@gmail.com>
2025-10-16 13:35:12 -0500

    example: added an example for a work-around of null value filtering.

--- apps-examples/simple-graphql-crud-app/src/suppliers/suppliers.service.ts
+++ apps-examples/simple-graphql-crud-app/src/suppliers/suppliers.service.ts
@@ -1,9 +1,11 @@
 import {
   CrudServiceFrom,
   CrudServiceStructure,
+  TypeOrmSelectQueryBuilder,
 } from '@solid-nestjs/typeorm-graphql-crud';
 import { Supplier } from './entities/supplier.entity';
 import { CreateSupplierDto, FindSupplierArgs, UpdateSupplierDto } from './dto';
+import { Context, DataRetrievalOptions } from '@solid-nestjs/typeorm';
 
 export const serviceStructure = CrudServiceStructure({
   entityType: Supplier,
@@ -28,4 +30,35 @@ export const serviceStructure = CrudServiceStructure({
   },
 });
 
-export class SuppliersService extends CrudServiceFrom(serviceStructure) {}
+export class SuppliersService extends CrudServiceFrom(serviceStructure) {
+  override getQueryBuilder(
+    context: Context,
+    args?: FindSupplierArgs,
+    options?: DataRetrievalOptions<Supplier>,
+  ): TypeOrmSelectQueryBuilder<Supplier> {
+    const queryBuilder = super.getQueryBuilder(context, args, options);
+
+    return this.additionalConditions(queryBuilder);
+  }
+
+  //this is needed for paginated queries with multiplying relations
+  override getNonMultiplyingPaginatedQueryBuilder(
+    context: Context,
+    args?: FindSupplierArgs,
+    options?: DataRetrievalOptions<Supplier>,
+  ): false | TypeOrmSelectQueryBuilder<Supplier> {
+    const queryBuilderOrFalse = super.getQueryBuilder(context, args, options);
+
+    if (!queryBuilderOrFalse) return queryBuilderOrFalse;
+
+    const queryBuilder = queryBuilderOrFalse;
+
+    return this.additionalConditions(queryBuilder);
+  }
+
+  additionalConditions(
+    queryBuilder: TypeOrmSelectQueryBuilder<Supplier>,
+  ): TypeOrmSelectQueryBuilder<Supplier> {
+    return queryBuilder.andWhere('"supplier_products"."id" is not null');
+  }
+}
